26bc1743e94c68e96d6203a58a8963dce5cd18f6
Ricardo Trigo <ricardo.and@hotmail.es>
2019-11-27 09:34:30 +0100

    test conversion chaine n caracteres identiques

--- tests/features/test_calc.py
+++ tests/features/test_calc.py
@@ -6,9 +6,15 @@ def test_input_1_caractere():
     assert CalcRomains.convertir_caractere("M") == 1000
     assert CalcRomains.convertir_caractere("X") == 10
 
-def test_input_2_caracteres():
+def test_input_2_caracteres_identiques():
     assert CalcRomains.convertir_chaine("II") == 2
     assert CalcRomains.convertir_chaine("XX") == 20
 
 def test_input_n_caracteres():
-    assert CalcRomains.convertir_chaine_avec_soustraction("MCMXLIV") == 1944
+    assert CalcRomains.convertir_chaine("MMCLXI") == 2161
+    assert CalcRomains.convertir_chaine("MMDCLXXVI") == 2676
+
+def test_input_n_caracteres_identiques():
+    assert CalcRomains.convertir_chaine("MMMMMM") == 6000
+    assert CalcRomains.convertir_chaine("XXXXXXXXXX") == 100
+
